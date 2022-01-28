bda9f1e20dc4d291e217ca5abc6947008efcb266
Ultra Zacky <ultrazacky@gmail.com>
2022-01-28 12:38:29 -0800

    add back in branch. object is now retaining value

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,28 @@
-function PizzaProcessor() {
-  this.pizza = {};
-  this.currentID = 0;
-}
-
-// PizzaProcessor.prototype.orderNumber = function() {
-//   this.currentID += 1;
-//   return this.currentID; 
-// };
-
-// PizzaProcessor.prototype.processOrder = function(pizza) {
-//   pizza.id = this.orderNumber();
-//   this.pizza[pizza.id] = pizza;
-// };
-
-// PizzaProcessor.prototype.findOrder = function(id) {
-//   if (this.pizza[id] != undefined) {
-//     return this.pizza[id];
-//   }
-//   return false;
-// };
-
-// function displayOrderDetails(customerOrderToDisplay) {
-//   let htmlForCustomerOrder = "";
-//   Object.keys(customerOrderToDisplay.pizza).foreach(function(key) {
-//     const pizza = customerOrderToDisplay.processOrder(key);
-//     htmlForCustomerOrder += pizza.id + pizza.size + pizza.toppings;
-//   });
-//   alert(pizza);
-// }
-
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
+let orderDetails = new Pizza();
 
 function pushValue() {
-  const pizsize = $("#size").val();
-    const piztoppings = $("#toppings").val();
+  const pizzaSize = $("input#size").val();
+  console.log(pizzaSize);
+    const pizzaToppings = $("input#toppings").val();
+    console.log(pizzaToppings);
     
 
-    if (pizsize === "small" && piztoppings === "pepperoni") {
-      let orderDetails = new Pizza(pizsize, piztoppings);
-
-      alert(orderDetails)
+    if (pizzaSize === "small" && pizzaToppings === "pepperoni") {
+      orderDetails.size = "Small";
+      orderDetails.toppings = "Pepperoni"
+      console.log(orderDetails);
     }
 }
 
 $(document).ready(function(){
   $("button#getPizza").click(function(event){
     event.preventDefault();
-    const customerOrder = new PizzaProcessor();
+    // const customerOrder = new PizzaProcessor();
 
     pushValue()
 
