b2dc64f186de36af376172c8713370238a147c0a
Ultra Zacky <ultrazacky@gmail.com>
2022-01-29 11:57:34 -0800

    add CSS styling to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,41 @@
-function Pizza(size, toppings) {
+function Pizza(size, toppings, ) {
   this.size = size;
   this.toppings = {};
 }
 
 let orderDetails = new Pizza();
 
-function Add(num1, num2, num3) {
-  const result = num1 + num2 + num3;
-  console.log(result);
-}
+// Pizza.prototype.concatThisOrder = function() {
+
+//   let yourOrder = "<h2>One " + orderDetails.size + " pizza with " + orderDetails.toppings + "comes out to $" + //price// + ".</h2>"
+
+//   };
+
+// function Add(num1, num2, num3) {
+//   const result = num1 + num2 + num3;
+//   console.log(result);
+// }
 
 function attachSizeVal() {
   const size = $("select#sizeS").val();
 
-  if (size === '1') {
+  if (size === '6') {
     orderDetails.size = "Small";
 
-  } else if (size === '2') {
+  } else if (size === '12') {
     orderDetails.size = "Medium";
 
-  } else if (size === '3') {
+  } else if (size === '24') {
     orderDetails.size = "Gigantor"
   }
 }
 
 function attachToppingsVal() {
 
-  let topping1 = parseInt($("select#pep").val()); 
-  let topping2 = parseInt($("select#oliv").val()); 
+  let topping1 = parseInt($("select#pep").val());
+  let topping2 = parseInt($("select#oliv").val());
   let topping3 = parseInt($("select#mush").val());
 
-  console.log(topping1 + topping2 + topping3)
-  
   if ((topping1 + topping2 + topping3) === 1) {
     orderDetails.toppings = "Pepperoni";
 
@@ -46,27 +50,25 @@ function attachToppingsVal() {
 
   } else if ((topping1 + topping2 + topping3) === 5) {
     orderDetails.toppings = ["Pepperoni", "Mushrooms"];
-    
+
   } else if ((topping1 + topping2 + topping3) === 6) {
     orderDetails.toppings = ["Olives", "Mushrooms"];
-    
+
   } else if ((topping1 + topping2 + topping3) === 7) {
     orderDetails.toppings = ["Pepperoni", "Olives", "Mushrooms"];
-    
+
   } else {
     orderDetails.toppings = "Cheese";
   }
 }
 
-
-$(document).ready(function(){
-  $("button#getPizza").click(function(event){
+$(document).ready(function() {
+  $("button#getPizza").click(function(event) {
     event.preventDefault();
 
     attachSizeVal();
     attachToppingsVal();
-    console.log(orderDetails);
-    
+    console.log(orderDetails.size + " " + orderDetails.toppings);
 
   });
 
