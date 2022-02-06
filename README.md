2060334ad50681cefcf32c06ffe49d4ebf5fda07
Ultra Zacky <ultrazacky@gmail.com>
2022-02-05 18:21:59 -0800

    add refactoring to code, change to README and HTML

--- README.md
+++ README.md
@@ -31,137 +31,32 @@ to go to GitHub page for the Repository.
  This should put a cloned folder of the document on your desktop. Next open the folder and click on the file name index.html to open it in a browser and view it.
 #
 
-### Test Driven Development
+## Test Driven Development (TDD)
 #
 
-Decription: Pizza()
+### Describe: Pizza()
+#
+Test: "It should create a new pizza object"
 
-Test: "this should store size and toppings"
 Code:
-function Pizza(size, toppings, price) {
-  this.size = size;
-  this.toppings = toppings;
-  this.price = price;
-}
-Expected Result: Pizza("Large", "Pepperoni) = Pizza [Size: "Large" Toppings: "Pepperoni"]
+const yourPizza = new Pizza("Small", "Pepperoni")
 
-Description: attachSizeVal()
+Expected Output: Pizza {size: "Small" toppings: "Pepperoni" price: }
 
-Test: "It should compare options on submit and store those values into Object"
+#
+### Describe: Pizza.prototype.addPrice()
+#
+Test: "It should attach price to pizza object"
+
 Code:
-let orderDetails = new Pizza();
-function attachSizeVal() {
-  const size = $("select#sizeS").val();
+yourPizza.addPrice()
 
-  if (size === '8') {
-    orderDetails.size = "Small";
-    orderDeailts.price = '8';
-
-  } else if (size === '16') {
-    orderDetails.size = "Medium";
-    orderDetails.price = '16';
-
-  } else if (size === '22') {
-    orderDetails.size = "Gigantor"
-    orderDetails.price = '22'
-
-  } else {
-    orderDetails.size = "slice"
-    orderDetails.price = '3'
-    
-  }
-}
-Expected Result: Pizza {size: 'Small' price: '8'}
-
-Description: attachToppingsVal()
-
-Test: "It should look for all checked boxes and add items to object"
-Code: 
-
-function attachToppingsVal() {
-  
-const pepperoni = $("input#toppingPep").val(); 
-const olives = $("input#toppingOli").val(); 
-const mushrooms = $("input#toppingMush").val();
-const sausage = $("input#toppingSaug").val();
-const beef = $("input#toppingBef").val();
-const onions = $("input#toppingOni").val();
-const peppers = ("input#toppingBell").val();
-const anchovies = $("input#toppingAnch").val();
-
-function attachToppingsVal() {
-
-  let topping1 = parseInt($("select#pep").val());
-  let topping2 = parseInt($("select#oliv").val());
-  let topping3 = parseInt($("select#mush").val());
-
-
-  if ((topping1 + topping2 + topping3) === 1) {
-    orderDetails.toppings = "Pepperoni";
-    let addUpPrice = parseInt(orderDetails.price[0]) + 2;
-    orderDetails.price = addUpPrice.toString();
-
-  } else if ((topping1 + topping2 + topping3) === 2) {
-    orderDetails.toppings = "Olive";
-    let addUpPrice = parseInt(orderDetails.price[0]) + 2;
-    orderDetails.price = addUpPrice.toString();
-  
-  } else if ((topping1 + topping2 + topping3) === 4) {
-    orderDetails.toppings = "Mushroom";
-    let addUpPrice = parseInt(orderDetails.price[0]) + 2;
-    orderDetails.price = addUpPrice.toString();
-
-  } else if ((topping1 + topping2 + topping3) === 3) {
-    orderDetails.toppings = ["Pepperoni", "Olive"];
-    let addUpPrice = parseInt(orderDetails.price[0]) + 4;
-    orderDetails.price = addUpPrice.toString();
-
-  } else if ((topping1 + topping2 + topping3) === 5) {
-    orderDetails.toppings = ["Pepperoni", "Mushroom"];
-    let addUpPrice = parseInt(orderDetails.price[0]) + 4;
-    orderDetails.price = addUpPrice.toString();
-
-  } else if ((topping1 + topping2 + topping3) === 6) {
-    orderDetails.toppings = ["Olive", "Mushrooms"];
-    let addUpPrice = parseInt(orderDetails.price[0]) + 4;
-    orderDetails.price = addUpPrice.toString();
-
-  } else if ((topping1 + topping2 + topping3) === 7) {
-    orderDetails.toppings = ["Pepperoni", "Olive", "Mushroom"];
-    let addUpPrice = parseInt(orderDetails.price[0]) + 6;
-    orderDetails.price = addUpPrice.toString();
-
-  } else {
-    orderDetails.toppings = "Cheese";
-
-  }
-}
-Expected Result: Pizza {size: 'Small' toppings: 'Pepperoni' price: '10'} 
-
-Description: concatThisOrder()
-
-Test: "It should concat the size, toppings and price"
-Code:
-Pizza.prototype.concatThisOrder = function() {
-
-  if (orderDetails.size != 'slice') {
-    
-    $("h5#total").html("<h5 id='total'>Your total: \"1 " + orderDetails.size + " " + orderDetails.toppings + " pizza comes out to $" + "\" </h5>");
-
-  } else {
-
-    $("h5#total").html("<h5 id='total'>Your total: \"1 " + orderDetails.size + " of " + orderDetails.toppings + " comes out to $3" + "\" </h5>");
-
-  }
-
-  };
-Expected Result: Pizza 1 Small pizza with Olives comes out to $12."
-
-
+Expected Output: Pizza {size: "Small" toppings: "Pepperoni" price: "10"}
 
 #
 
-## Link to User Page: https://github.com/Ultra-Zactimus
+## Link to User Page: 
+https://github.com/Ultra-Zactimus
 #
 
 ## Known Bugs:
--- js/scripts.js
+++ js/scripts.js
@@ -1,119 +1,66 @@
 function Pizza(size, toppings, price) {
   this.size = size;
-  this.toppings = {};
-  this.price = {};
+  this.toppings = toppings;
+  this.price = price;
 }
 
-let orderDetails = new Pizza();
+Pizza.prototype.addPrice = function() {
 
-Pizza.prototype.concatThisOrder = function() {
-
-    $("h1#total").html("<h1 id='total'>Your total: \"1 " + orderDetails.size + " of " + orderDetails.toppings + " comes out to $" + orderDetails.price + "\" </h1>");
-
-  };
-
-function attachSizeVal() {
-  const size = $("select#sizeS").val();
-
-  if (size === '8') {
-    orderDetails.size = "Small";
-    orderDetails.price = '8';
-
-  } else if (size === '16') {
-    orderDetails.size = "Medium";
-    orderDetails.price = "16";
-
-  } else if (size === '22') {
-    orderDetails.size = "Gigantor"
-    orderDetails.price = "22";
-
-  } else if (size === '0'){
-    orderDetails.size = "slice"
-    orderDetails.price = '3';
-    
+  if (this.size === undefined) {
+    this.size = "slice of "
+    this.price = "3"
   }
-}
-
-function attachToppingsVal() {
-
-  let topping1 = parseInt($("select#pep").val());
-  let topping2 = parseInt($("select#oliv").val());
-  let topping3 = parseInt($("select#mush").val());
-  let grabValues = orderDetails.price.toString().split();
-  let grabNumbers = parseInt(...grabValues);
-  
-  // console.log(orderDetails);
-
-
-  if ((topping1 + topping2 + topping3) === 1) {
-    orderDetails.toppings = "Pepperoni";
-    grabNumbers = (grabNumbers+2);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-
-  } else if ((topping1 + topping2 + topping3) === 2) {
-    orderDetails.toppings = "Olive";
-    grabNumbers = (grabNumbers+2);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-    
-  
-  } else if ((topping1 + topping2 + topping3) === 4) {
-    orderDetails.toppings = "Mushroom";
-    grabNumbers = (grabNumbers+2);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-    
-
-  } else if ((topping1 + topping2 + topping3) === 3) {
-    orderDetails.toppings = ["Pepperoni", "Olive"];
-    grabNumbers = (grabNumbers+4);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-    
-
-  } else if ((topping1 + topping2 + topping3) === 5) {
-    orderDetails.toppings = ["Pepperoni", "Mushroom"];
-    grabNumbers = (grabNumbers+4);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-    
-
-  } else if ((topping1 + topping2 + topping3) === 6) {
-    orderDetails.toppings = ["Olive", "Mushrooms"];
-    grabNumbers = (grabNumbers+4);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-    
-
-  } else if ((topping1 + topping2 + topping3) === 7) {
-    orderDetails.toppings = ["Pepperoni", "Olive", "Mushroom"];
-    grabNumbers = (grabNumbers+6);
-    let stringNum = grabNumbers.toString();
-    orderDetails.price = stringNum;
-    
-
-  } else {
-    orderDetails.toppings = "Cheese";
+  if (this.toppings === undefined) {
+    this.toppings = "Cheese"
   }
-  
-}
+
+  if (this.size === 'Gigantor' && this.toppings === 'Pepperoni' || this.size === 'Gigantor' && this.toppings === 'Mushrooms' || this.size === 'Gigantor' && this.toppings === 'Olives' || this.size === 'Gigantor' && this.toppings === 'Cheese') {
+    this.price = "24";
+
+  } else if (this.size === 'Gigantor' && this.toppings === 'Pepperoni and Mushrooms' || this.size === 'Gigantor' && this.toppings === 'Mushrooms and Olives' || this.size === 'Gigantor' && this.toppings === 'Pepperoni and Olives') {
+    this.price = "26";
+
+  } else if (this.size === 'Gigantor' && this.toppings === 'Pepperoni, Mushrooms and Olives') {
+    this.price = "28";
+
+  } else if (this.size === 'Medium' && this.toppings === 'Pepperoni' || this.size === 'Medium' && this.toppings === 'Mushrooms' || this.size === 'Medium' && this.toppings === 'Olives' || this.size === 'Medium' && this.toppings === 'Cheese') {
+    this.price = "16";
+
+  } else if (this.size === 'Medium' && this.toppings === 'Pepperoni and Mushrooms' || this.size === 'Medium' && this.toppings === 'Mushrooms and Olives' || this.size === 'Medium' && this.toppings === 'Pepperoni and Olives') {
+    this.price = "18";
+
+  } else if (this.size === 'Medium' && this.toppings === 'Pepperoni, Mushrooms and Olives') {
+    this.price = "20";
+
+  } else if (this.size === 'Small' && this.toppings === 'Pepperoni' || this.size === 'Small' && this.toppings === 'Mushrooms' || this.size === 'Small' && this.toppings === 'Olives' || this.size === 'Small' && this.toppings === 'Cheese') {
+    this.price = "10";
+
+  } else if (this.size === 'Small' && this.toppings === 'Pepperoni and Mushrooms' || this.size === 'Small' && this.toppings === 'Mushrooms and Olives' || this.size === 'Small' && this.toppings === 'Pepperoni and Olives') {
+    this.price = "12";
+
+  } else if (this.size === 'Small' && this.toppings === 'Pepperoni, Mushrooms and Olives') {
+    this.price = "14";
+  }
+};
 
 $(document).ready(function() {
-  $("button#getPizza").click(function(event) {
-    event.preventDefault();
-
-    attachSizeVal();
-    attachToppingsVal();
-    orderDetails.concatThisOrder();
-    $("#selections").hide();
-    $("#showTotal").show();
+  let orderDetails = new Pizza();
+  $("#sizes").on('change', function() {
+    orderDetails.size = $("#sizes").children("option:selected").text();
+    console.log(orderDetails);
   });
-
-  $("button#return").click(function(reset){
-    document.getElementById("#selections");
-    $("#showTotal").hide();
-    $("#selections").show();
+  $("#toppings").on('change', function() {
+    orderDetails.toppings = $("#toppings").children("option:selected").text();
+    console.log(orderDetails);
   });
-
+  $("#getPizza").on('click', function() {
+    orderDetails.addPrice();
+    $("#getPizza").hide();
+    $("#total").show();
+    $("#return").show();
+    $("#total").html("1 " + orderDetails.size + " " + orderDetails.toppings + " pizza comes out to $" + orderDetails.price);
+  });
+  $("#return").on('click', function() {
+    location.reload(true);
+  });
 });
